48b7aa6d0df7cb058afc919cb8c7e5d959e7bfce
Heber Mattos <heber.mattos@ilegra.com>
2020-10-27 15:39:11 -0300

    um-servico

--- servico/Program.cs
+++ servico/Program.cs
@@ -19,7 +19,7 @@ namespace servico
                              {
                                  e.Consumer<ProcessarVisualizacaoImagem>();
                              });
-                        });
+                         });
         }
     }
 }
