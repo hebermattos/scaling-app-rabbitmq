64daa76efe4350c27668f377841d7e91d45e76f2
Heber Mattos <heber.mattos@ilegra.com>
2020-10-27 16:19:33 -0300

    um-servico

--- servico/Program.cs
+++ servico/Program.cs
@@ -14,6 +14,7 @@ namespace servico
                                 host.Username("guest");
                                 host.Password("guest");
                             });
+                            
                             config.ReceiveEndpoint("visualicaco.imagem", e =>
                             {
                                 e.Consumer<ProcessarVisualizacaoImagem>();
